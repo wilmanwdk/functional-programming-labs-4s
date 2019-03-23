d6246cdbbedfc6599f31897957bd34fe0e218395
Andrew Makarov <wilmanwdk@hotmail.com>
2019-03-23 10:55:55 +0200

    lab5

--- lab5-1.hs
+++ lab5-1.hs
@@ -6,3 +6,16 @@ solveQuadratic a b c
     | b*b-4*a*c > 0 = [(-b+sqrt(b*b-4*a*c))/(2*a), (-b-sqrt(b*b-4*a*c))/(2*a)]
     | b*b-4*a*c == 0 = [(-b+sqrt(b*b-4*a*c))/(2*a)]
     | otherwise = []
+
+main = do
+    putStrLn "ax^2+bx+c"
+    putStr "a = "
+    inputA <- getLine
+    putStr "b = "
+    inputB <- getLine
+    putStr "c = "
+    inputC <- getLine
+    let a = (read inputA :: Float)
+    let b = (read inputB :: Float)
+    let c = (read inputC :: Float)
+    print (solveQuadratic a b c)
